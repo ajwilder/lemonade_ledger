ff15cd7ba02db2ea4f891fbb76b539756de8fca1
Alan Wilder <wilderalan@gmail.com>
2018-04-21 18:47:28 -0400

    add exception_notification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,11 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+  :email => {
+    :deliver_with => :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+    :email_prefix => "[Error] ",
+    :sender_address => %{"notifier" <notifier@example.com>},
+    :exception_recipients => %w{wilderalan@gmail.com}
+  }
 end
